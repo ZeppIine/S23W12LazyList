0299cae0caa26af8550171d4648fd42c42a9abad
ZeppIine <kim0904min@gmail.com>
2023-11-22 16:48:41 +0900

    use Song data class

--- app/src/main/java/kr/ac/kumoh/ce/s20180147/s23w12lazylist/MainActivity.kt
+++ app/src/main/java/kr/ac/kumoh/ce/s20180147/s23w12lazylist/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.MaterialTheme
@@ -24,9 +25,18 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import kr.ac.kumoh.ce.s20180147.s23w12lazylist.ui.theme.S23W12LazyListTheme
 
+data class Song(var title: String, var singer: String)
+private val songs = mutableListOf<Song>()
+
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        repeat(10) {
+            songs.add(Song("상냥함의 기억", "윤하"))
+            songs.add(Song("Thanks to", "윤하"))
+            songs.add(Song("에브리데이 함께", "아로나"))
+        }
 
         setContent {
             MyScreen()
@@ -57,15 +67,15 @@ fun SingerText(title: String) {
 }
 
 @Composable
-fun SongItem(index: Int) {
+fun SongItem(song: Song) {
     Column(
         modifier = Modifier
             .fillMaxWidth()
             .background(Color(0xfffffcc))
             .padding(16.dp)
     ) {
-        TitleText("노래 $index")
-        SingerText("이 노래를 부른 가수는 ${index}입니다")
+        TitleText("노래 ${song.title}")
+        SingerText("이 노래를 부른 가수는 ${song.singer} 입니다")
     }
 }
 @Composable
@@ -74,8 +84,8 @@ fun MyList() {
         verticalArrangement = Arrangement.spacedBy(8.dp),
         contentPadding = PaddingValues(8.dp)
     ) {
-        items(30) {
-            SongItem(it)
+        items(songs)  {
+                song -> SongItem(song)
         }
     }
 }
