a5ad0b3ab04a929791fcad7685568712e0730947
ZeppIine <kim0904min@gmail.com>
2023-11-22 16:32:29 +0900

    add LazyList

--- app/src/main/java/kr/ac/kumoh/ce/s20180147/s23w12lazylist/MainActivity.kt
+++ app/src/main/java/kr/ac/kumoh/ce/s20180147/s23w12lazylist/MainActivity.kt
@@ -3,8 +3,13 @@ package kr.ac.kumoh.ce.s20180147.s23w12lazylist
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
@@ -13,13 +18,16 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import kr.ac.kumoh.ce.s20180147.s23w12lazylist.ui.theme.S23W12LazyListTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContent {
             MyScreen()
         }
@@ -44,10 +52,30 @@ fun TitleText(title: String) {
 }
 
 @Composable
+fun SingerText(title: String) {
+    Text(title, fontSize = 20.sp)
+}
+
+@Composable
+fun SongItem(index: Int) {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .background(Color(0xfffffcc))
+            .padding(16.dp)
+    ) {
+        TitleText("노래 $index")
+        SingerText("이 노래를 부른 가수는 ${index}입니다")
+    }
+}
+@Composable
 fun MyList() {
-    LazyColumn {
+    LazyColumn(
+        verticalArrangement = Arrangement.spacedBy(8.dp),
+        contentPadding = PaddingValues(8.dp)
+    ) {
         items(30) {
-            TitleText("노래 $it")
+            SongItem(it)
         }
     }
 }
