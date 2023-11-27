0977f73e70b265d983e5fc5d5cdad4de7942f80a
ZeppIine <kim0904min@gmail.com>
2023-11-27 15:34:49 +0900

    modify to use backend

--- app/src/main/java/kr/ac/kumoh/ce/s20180147/s23w12lazylist/MainActivity.kt
+++ app/src/main/java/kr/ac/kumoh/ce/s20180147/s23w12lazylist/MainActivity.kt
@@ -3,6 +3,7 @@ package kr.ac.kumoh.ce.s20180147.s23w12lazylist
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -12,46 +13,38 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import kr.ac.kumoh.ce.s20180147.s23w12lazylist.ui.theme.S23W12LazyListTheme
 
-data class Song(var title: String, var singer: String)
-private val songs = mutableListOf<Song>()
-
 class MainActivity : ComponentActivity() {
+    private val viewModel: SongViewModel by viewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        repeat(10) {
-            songs.add(Song("상냥함의 기억", "윤하"))
-            songs.add(Song("Thanks to", "윤하"))
-            songs.add(Song("에브리데이 함께", "아로나"))
-        }
-
         setContent {
-            MyScreen()
+            MyScreen(viewModel)
         }
     }
 }
 
 @Composable
-fun MyScreen() {
+fun MyScreen(viewModel: SongViewModel) {
+    val songList by viewModel.songList.observeAsState(emptyList())
     S23W12LazyListTheme {
         Surface(
             modifier = Modifier.fillMaxSize(),
             color = MaterialTheme.colorScheme.background
         ) {
-            MyList()
+            MyList(songList)
         }
     }
 }
@@ -74,17 +67,17 @@ fun SongItem(song: Song) {
             .background(Color(0xfffffcc))
             .padding(16.dp)
     ) {
-        TitleText("노래 ${song.title}")
-        SingerText("이 노래를 부른 가수는 ${song.singer} 입니다")
+        TitleText(song.title)
+        SingerText("이 노래를 부른 가수는 ${song.singer}입니다")
     }
 }
 @Composable
-fun MyList() {
+fun MyList(list: List<Song>) {
     LazyColumn(
         verticalArrangement = Arrangement.spacedBy(8.dp),
         contentPadding = PaddingValues(8.dp)
     ) {
-        items(songs)  {
+        items(list)  {
                 song -> SongItem(song)
         }
     }
