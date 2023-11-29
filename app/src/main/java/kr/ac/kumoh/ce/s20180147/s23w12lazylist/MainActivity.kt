6166b15c96fa1086c8e9a3e8dbeaafc01123d272
ZeppIine <kim0904min@gmail.com>
2023-11-29 16:40:44 +0900

    add Card

--- app/src/main/java/kr/ac/kumoh/ce/s20180147/s23w12lazylist/MainActivity.kt
+++ app/src/main/java/kr/ac/kumoh/ce/s20180147/s23w12lazylist/MainActivity.kt
@@ -4,7 +4,9 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
+import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.IntrinsicSize
@@ -19,14 +21,20 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
@@ -76,45 +84,67 @@ fun SingerText(singer: String) {
 }
 
 @Composable
-fun SongItem(song: Song) {
-    Row(
-        modifier = Modifier
-            .fillMaxWidth()
-            .height(IntrinsicSize.Min)
-            .background(Color(0xfffffcc))
-            .padding(16.dp)
-    ){
-        AsyncImage(
-            model = "https://picsum.photos/300/300?random=${song.id}",
-            contentDescription = "노래 앨범 이미지",
-            contentScale = ContentScale.Crop,
+fun LyricsText(singer: String) {
+    Text(singer, fontSize = 20.sp)
+}
+
+@Composable
+fun SongItem(index: Int, song: Song) {
+    var expanded by remember { mutableStateOf(false) }
+
+    Card(
+        modifier = Modifier.clickable { expanded = !expanded },
+        elevation = CardDefaults.cardElevation(8.dp)
+    ) {
+        Row(
             modifier = Modifier
-                .size(100.dp)
-//                .clip(CircleShape),
-                .clip(RoundedCornerShape(percent = 10)),
-        )
-        Spacer(modifier = Modifier.width(10.dp))
-        Column(
-            modifier = Modifier
-                .fillMaxSize(),
-            verticalArrangement = Arrangement.Center
+                .fillMaxWidth()
+                .height(IntrinsicSize.Min)
+//                .background(Color(0xfffffcc))
+                .padding(8.dp)
         ) {
-            Row {
-                TitleText(song.title)
+            AsyncImage(
+                model = "https://picsum.photos/300/300?random=${index}",
+                contentDescription = "노래 앨범 이미지",
+                contentScale = ContentScale.Crop,
+                modifier = Modifier
+                    .size(80.dp)
+//                .clip(CircleShape),
+                    .clip(RoundedCornerShape(percent = 10)),
+            )
+            Spacer(modifier = Modifier.width(16.dp))
+            Column(
+                modifier = Modifier
+                    .fillMaxSize(),
+                verticalArrangement = Arrangement.SpaceAround
+            ) {
+                Row {
+                    TitleText(song.title)
 //                RatingInt(song.rating)
+                }
+                SingerText(song.singer)
             }
-            SingerText(song.singer)
+        }
+        AnimatedVisibility(
+            modifier = Modifier
+                .fillMaxWidth()
+                .background(Color(0xFF39C5BB))
+                .padding(8.dp),
+            visible = expanded
+        ) {
+            LyricsText(" ${song.lyrics}")
         }
     }
 }
+
 @Composable
 fun MyList(list: List<Song>) {
     LazyColumn(
         verticalArrangement = Arrangement.spacedBy(8.dp),
         contentPadding = PaddingValues(8.dp)
     ) {
-        items(list)  {
-                song -> SongItem(song)
+        itemsIndexed(list)  {
+                index, song -> SongItem(index, song)
         }
     }
 }
