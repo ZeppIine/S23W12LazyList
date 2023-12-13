b54adfa8fc9835d18d71a70f1508b0e98713b60f
ZeppIine <kim0904min@gmail.com>
2023-12-13 15:18:41 +0900

    add youtube search

--- app/src/main/java/kr/ac/kumoh/ce/s20180147/s23w12lazylist/SongScreen.kt
+++ app/src/main/java/kr/ac/kumoh/ce/s20180147/s23w12lazylist/SongScreen.kt
@@ -1,7 +1,10 @@
 package kr.ac.kumoh.ce.s20180147.s23w12lazylist
 
+import android.content.Intent
 import android.graphics.drawable.Icon
+import android.net.Uri
 import android.widget.RatingBar
+import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -20,6 +23,7 @@ import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.Button
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Star
 import androidx.compose.material3.Card
@@ -30,11 +34,17 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.geometry.CornerRadius
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Path
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.core.content.ContextCompat.startActivity
 import androidx.navigation.NavController
 import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
@@ -161,6 +171,8 @@ fun MyList(navController: NavController ,list: List<Song>) {
 
 @Composable
 fun MyDetail(songList: Song){
+    val context = LocalContext.current
+
     Column (
         modifier = Modifier
             .padding(16.dp)
@@ -212,5 +224,47 @@ fun MyDetail(songList: Song){
                 lineHeight = 35.sp
             )
         }
+        Spacer(modifier = Modifier.height(32.dp))
+        Button(
+            onClick = {
+            val intent = Intent(
+                Intent.ACTION_VIEW,
+                Uri.parse("https://www.youtube.com/results?search_query=노래방+${songList.title}")
+            )
+            startActivity(context, intent, null)
+        }) {
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.Center,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                YoutubeIcon()
+                Spacer(modifier = Modifier.width(16.dp))
+                Text("노래방 검색", fontSize = 30.sp)
+            }
+        }
+    }
+}
+
+@Composable
+fun YoutubeIcon() {
+    Canvas(
+        modifier = Modifier
+            .size(70.dp)
+    ) {
+
+        val path = Path().apply {
+            moveTo(size.width * .43f, size.height * .38f)
+            lineTo(size.width * .72f, size.height * .55f)
+            lineTo(size.width * .43f, size.height * .73f)
+            close()
+        }
+        drawRoundRect(
+            color = Color.Red,
+            cornerRadius = CornerRadius(40f, 40f),
+            size = Size(size.width, size.height * .70f),
+            topLeft = Offset(size.width.times(.0f), size.height.times(.20f))
+        )
+        drawPath(color = Color.White, path = path)
     }
 }
